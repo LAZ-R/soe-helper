671eab62e36b81492a121bcf24d34df4b78d8d5f
LAZ-R <crazydrawer62@gmail.com>
2024-01-06 13:50:38 +0100

    v 0.0.2 : gray theme

--- js/app.js
+++ js/app.js
@@ -40,11 +40,35 @@ main.innerHTML = `
   <div class="rounds-area actual-round-area">vague actuelle</div>
   <div class="rounds-area next-round-area">vague suivante</div>
   <div class="central-area">Centre</div>
-  <div class="player-area player-1-area">Player 1</div>
-  <div class="player-area player-2-area">Player 2</div>
-  <div class="district-area footlight-area">Footlight</div>
-  <div class="district-area waterfront-area">Waterfront</div>
-  <div class="district-area canal-area">Canal</div>
+  <div class="player-area player-1-area">
+    <div class="area-title"><span>JOUEUR 1</span></div>
+    <div class="area-content">
+      <span>content</span>
+    </div>
+  </div>
+  <div class="player-area player-2-area">
+    <div class="area-title"><span>JOUEUR 2</span></div>
+    <div class="area-content">
+      <span>content</span>
+    </div>
+  </div>
+  <div class="district-area footlight-area">
+    <div class="area-title"><span>FOOTLIGHT</span></div>
+    <div class="area-content">
+      <span>content</span>
+    </div>
+  </div>
+  <div class="district-area waterfront-area">
+    <div class="area-title"><span>WATERFRONT</span></div>
+    <div class="area-content">
+      <span>content</span></div>
+  </div>
+  <div class="district-area canal-area">
+    <div class="area-title"><span>CANAL</span></div>
+    <div class="area-content">
+      <span>content</span>
+    </div>
+  </div>
 `;
 
 /* const footer = document.getElementById('footer');
